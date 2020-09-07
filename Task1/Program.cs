7664a960f780653dcc7f57f64b9f117710caccee
Denys Vynohradnyi <denv0808@gmail.com>
2020-09-08 01:44:25 +0300

    last

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,38 +1,22 @@
 ﻿using System;
 using Task1.BL;
+using Task1.BL.Interfaces;
 
 namespace Task1
 {
 
     class Program
-    {   
+    {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
             IFileTxt reader = new FileManager();
             WalkerDirectories wd = new WalkerDirectories();
             ConsolManager cm = new ConsolManager((IWalkerDirectories)wd,reader);
-=======
-            ConsolManager cm = new ConsolManager();
-<<<<<<< HEAD
->>>>>>> parent of dc673c1... Fixed all
-=======
-            ConsolManager cm = new ConsolManager();
->>>>>>> parent of dc673c1... Fixed all
             string str;//Строка для обработки ответа пользователя.
             while (true)
             {
                 Console.Clear();
-<<<<<<< HEAD
-<<<<<<< HEAD
                 Console.WriteLine("{0} \t| Change disk: \"cd\", Back: \"..\", Full Path: \"fp\", Open File: \"open\", Exit: \"bye\"", PathManager.Path) ; //toolbar
-=======
-                Console.WriteLine("{0} \t| Change disk: \"cd\", Back: \"..\", Full Path: \"fp\", Open File: \"open\", Exit: \"bye\"", cm.getPath()) ; //toolbar
->>>>>>> parent of dc673c1... Fixed all
-=======
-                Console.WriteLine("{0} \t| Change disk: \"cd\", Back: \"..\", Full Path: \"fp\", Open File: \"open\", Exit: \"bye\"", cm.getPath()) ; //toolbar
->>>>>>> parent of dc673c1... Fixed all
 
                 wd.DisplayDirectories();
 
@@ -44,13 +28,6 @@ namespace Task1
                 cm.MenuBar(str);
 
                 wd.SearchDirectories(str);
-=======
-            while (true)
-            {
-                cm.Walk(out String str);
-
-                if (ConsolManager.isExite(out str)) return;
->>>>>>> parent of e10b476... Fixed
             }
         }
     }
--- Task1/Task1.BL/ConsolManager.cs
+++ Task1/Task1.BL/ConsolManager.cs
@@ -1,55 +1,29 @@
 ﻿using System;
+using System.IO;
 using Task1.BL.Interfaces;
 
 namespace Task1.BL
 {
     /// <summary>
-    /// Класс для отображение элементов в консоли
+    /// Класс для отображение элементов в консоли.
     /// </summary>
     public class ConsolManager
     {
         /// <summary>
-        /// Указатель на класс для чтения файла
+        /// Указатель на класс для работы с файлом.
         /// </summary>
-        private Reader _reader;
+        private IFileTxt _reader;
         /// <summary>
-<<<<<<< HEAD
-        /// Указатель на класс для работы с текстом
-=======
-        /// Указатель на класс для работы с текстом.
->>>>>>> parent of dc673c1... Fixed all
+        /// Указатель на класс для работы с директориями.
         /// </summary>
-        private TextWorker _fileText;
+        private IWalkerDirectories _walker;
         /// <summary>
-        /// Указатель на класс для работы с директориями
+        /// Конструтор класса ConsolManager.
         /// </summary>
-        private WalkerDirectories _walker;
-        /// <summary>
-        /// Конструтор класса ConsolManager
-        /// </summary>
-        public ConsolManager()
+        public ConsolManager(IWalkerDirectories walker, IFileTxt reader)
         {
-<<<<<<< HEAD
-            _walker = new WalkerDirectories();
-=======
-            _walker = new WalkerDirectories(); //инициализация папок и файлов
->>>>>>> parent of dc673c1... Fixed all
-        }
-        
-
-        /// <summary>
-        /// Отображение директории.
-        /// </summary>
-        public void DisplayDirectories()
-        {
-            _walker.DisplayDirectories();
-        }
-        /// <summary>
-        /// Отображение файлов.
-        /// </summary>
-        public void DisplayFiles()
-        {
-            _walker.DisplayFiles();
+            _walker = walker; //инициализация папок и файлов
+            _reader = reader;
         }
         /// <summary>
         /// Метод для обработки команд menu.
@@ -60,12 +34,12 @@ namespace Task1.BL
             switch (str.ToLower())
             {
                 case ".."://back
-                    for (int i = _walker.Path.Length - 2; i > 0; i--)
+                    for (int i = PathManager.Path.Length - 2; i > 0; i--)
                     {
-                        if (_walker.Path[i] == '\\')
+                        if (PathManager.Path[i] == '\\')        // обрезаем последнюю директорию
                         {
-                            _walker.SetPath(_walker.Path.Remove(i + 1, _walker.Path.Length - i - 1));
-                            _walker.SetDireketories(_walker.Path);
+                            PathManager.Path= PathManager.Path.Remove(i + 1, PathManager.Path.Length - i - 1);
+                            _walker.SetDirectories(PathManager.Path);
                             return;
                         }
                     }
@@ -76,38 +50,36 @@ namespace Task1.BL
                 case "fp"://Full path
                     Console.Write("Full Path directory: ");
                     str = Console.ReadLine();
-                    _walker.SetBackupPath(_walker.Path);
+                    PathManager.SetBackupPath(PathManager.Path);
                     //проверка, на тот случай, если пользватель хочет открыть файл указав полный путь
                     if (!str.EndsWith(".txt"))
                     {
                         try
                         {
-                            _walker.SetPath(str);
-                            _walker.SetDireketories(str);
+                            PathManager.Path=str;
+                            _walker.SetDirectories(str);
                             return;
                         }
                         catch (Exception)
                         {
                             Console.WriteLine("Wrong Path... Try again. \t*enter*");
                             Console.ReadLine();
-                            _walker.BackupPath();
+                            PathManager.BackupPath();
                             return;
                         }
                     }
                     else
                     {
-                        if(str.Contains("\\")) // проверка на наличие полного пути
+                        if(File.Exists(str))                        
                         {
-                            if (_reader == null) _reader = new Reader(str);
-                            _walker.SetPath(_reader.CutFileName(str)); //обрезаем от пути название файла
-
+                            PathManager.Path=Path.GetDirectoryName(str); //обрезаем от пути название файла
                             Console.Clear();
-                            OpenFile((IReadTxt)_reader);
+                            _reader.OpenFile(str);
                             return;
                         }
                     }
                     break;
-                case "open": //Open File
+                case "open": //Open File //Работае иначе от full path
                     Console.Write("File: ");
                     str = Console.ReadLine();
                     Console.WriteLine();
@@ -117,8 +89,7 @@ namespace Task1.BL
                     {
                         if (str.EndsWith(".txt"))
                         {
-                            if (_reader == null) _reader = new Reader(_walker.Path + str);
-                            OpenFile((IReadTxt)_reader);
+                            _reader.OpenFile(PathManager.Path + str);
                             return;
                         }
                     }
@@ -126,127 +97,26 @@ namespace Task1.BL
                     {
                         if (str.EndsWith(".txt"))
                         {
-                            if (_reader == null) _reader = new Reader(str);
-
-                            _walker.SetPath(_reader.CutFileName(str));
+                            PathManager.Path=Path.GetDirectoryName(str)+"\\";
                             Console.Clear();
-                            OpenFile((IReadTxt)_reader);
+                            _reader.OpenFile(str);
                             return;
                         }
                     }
                     break;
                 case "bye": //Exite
-                    if (isExite()) Environment.Exit(0);
+                    if (IsExite()) Environment.Exit(0);
                     break;
             }
-        }
-        /// <summary>
-        /// Поиск директории.
-        /// </summary>
-        /// <param name="nameDirectory">Название директории.</param>
-        public void SearchDirectories(string nameDirectory)
-        {
-            _walker.SearchDirectories(nameDirectory);
-        }
-        /// <summary>
-        /// Возвращает путь текущего местположения в директории
-        /// </summary>
-<<<<<<< HEAD
-        /// <returns>Строка</returns>
-=======
-        /// <returns>Строка.</returns>
->>>>>>> parent of dc673c1... Fixed all
-        public string getPath()
-        {
-            return _walker.Path;
-        }
-      
-        
-        /// <summary>
-        /// Приватный метод для работы с файлом формата txt.
-        /// </summary>
-        /// <param name="read">Экземпляр интерфейса IReadTxt.</param>
-        private void WorkWithFile(IReadTxt read)
-        {
-            string str;// Строка, для обработки ответа пользователя.
-            while (true)
-            {
-                Console.WriteLine(_fileText.GetText());
-                Console.Write(
-                   "1. Delete symbol or word.\n" +
-                   "2. Count words and every tenth word.\n" +
-                   "3. Backward third sentence.\n" +
-                   "4. Close file.\n" +
-                   "(number) :"
-                   );
-                str = Console.ReadLine();
-                if (int.TryParse(str, out int result))
-                {
-                    switch (result)
-                    {
-                        case 1:
-                            Console.WriteLine("Symbol or word: ");
-                            _fileText.Delete(Console.ReadLine());
-                            break;
-                        case 2:
-                            _fileText.CountWordsAndTenWords();
-                            break;
-                        case 3:
-                            _fileText.ThirdSentenceReverse();
-                            break;
-                        case 4:
-                            Console.WriteLine("Save changes ?(yes,no)");
-                            str = Console.ReadLine().ToLower();
-                            if (str == "yes" || str == "y")
-                            {
-                                read.CreateFile(_fileText.GetText()); //Creat backup
-                            }
-                            _reader = null; // delete )
-                            return;
-                        default:
-                            break;
-                    };
-                }
-                Console.WriteLine("\t\t *enter*");
-                Console.ReadKey();
-                Console.Clear();
-            }
-        }
-        /// <summary>
-        /// Приватный метод для чтения файла
-        /// </summary>
-        /// <param name="read">Экземпляр интерфейса IReadTxt.</param>
-        private void OpenFile(IReadTxt read)
-        {
-            _fileText = new TextWorker(read.ReadTxt());
+        }  
 
-            try
-            {
-                if (_fileText.Text[0] == "No" && _fileText.Text[1] == "file" && _fileText.Text[4] == "name")
-                {
-                    Console.WriteLine(_fileText.GetText());
-                    _reader = null;
-                    Console.ReadKey();
-                    return;
-                }
-                WorkWithFile(read);
-            }
-            catch(Exception)
-            {
-                WorkWithFile(read);
-            }
-        }
         /// <summary>
         /// Булевый метод для закрытия программы.
         /// </summary>
-<<<<<<< HEAD
-        /// <returns>Булевая переменная - закрывается ли программа</returns>
-=======
         /// <returns>Булевая переменная - закрывается ли программа</returns>    
->>>>>>> parent of dc673c1... Fixed all
-        private static bool isExite()
+        private static bool IsExite()
         {
-            String str;//Обработка ответа пользователя.
+            string str;//Обработка ответа пользователя.
             Console.Write("Close console? (yes,no): ");
             str = Console.ReadLine().ToLower();
             if (str == "yes" || str == "y")
@@ -259,7 +129,7 @@ namespace Task1.BL
                 return false;
             }
             Console.WriteLine("Mistake, try again");
-            return isExite();
+            return IsExite();
         }
     }
 }
