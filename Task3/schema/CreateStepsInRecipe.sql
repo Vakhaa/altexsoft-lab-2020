3fe06353110b9054c5decd747f483198449499e8
Denys Vynohradnyi <denv0808@gmail.com>
2020-09-20 17:59:07 +0300

    Fixed

--- Task3/schema/CreateStepsInRecipe.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROC TabelStepsInRecipe
-    @stepsTable NVARCHAR(50) OUTPUT
-AS
-BEGIN
-    SET @stepsTable =CONCAT('StepsInRecipe_',(SELECT MAX(Id)+1 FROM Recipes))
-    DECLARE @cmd nvarchar(1000)
-    SET @cmd = 'CREATE TABLE dbo.'+@stepsTable+'(Id INT IDENTITY PRIMARY KEY, Description NVARCHAR(500))'
-    EXEC(@cmd);
-END;
