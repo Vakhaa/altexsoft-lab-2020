ddb72b117fe8ddfd96009124cbf1a552d859c0e3
Denys Vynohradnyi <denv0808@gmail.com>
2020-09-29 15:51:56 +0300

    Fixed names

--- Task3/queries/task.sql
+++ Task3/queries/task.sql
@@ -9,15 +9,14 @@
         INNER JOIN CTE_Categories AS c2  
         ON c1.ParentId = c2.Id   
 ),
-FirthLayerCategories (Id, InheritorId, Name)AS
+FirstLayerCategories (Id, InheritorId, Name)AS
 (
     SELECT c1.Id AS Id, c.Id AS InheritorId, c1.Name AS Name FROM CTE_Categories AS c INNER JOIN CTE_Categories AS c1 ON c.ParentId = c1.Id
     WHERE c.LevelLayer<2
 )
 
 SELECT Recipes.Name As Recipe , Ingredients.Name As Ingredient, CountIngredient FROM	
-		Recipes INNER JOIN FirthLayerCategories AS c ON Recipes.CategoryId = c.InheritorId,
+		Recipes INNER JOIN FirstLayerCategories AS c ON Recipes.CategoryId = c.InheritorId,
 		Ingredients INNER JOIN IngredientsInRecipe AS IR1 ON IR1.IngredientId = Ingredients.Id
 		WHERE IR1.RecipeId =Recipes.Id AND c.Id = 3
 		GROUP BY Recipes.Name , Ingredients.Name , CountIngredient;
-
