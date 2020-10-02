ed08b36eed20db1c9928526a3ae2f22bcc88e8c9
Denys Vynohradnyi <denv0808@gmail.com>
2020-10-02 13:32:15 +0300

    Change parentId = 2 on parentId = 3

--- Task3/queries/task.sql
+++ Task3/queries/task.sql
@@ -13,7 +13,7 @@ SELECT Recipe,Ingredient, CountIngredient, Iterator FROM (
         SELECT Recipes.Name As Recipe , Ingredients.Name As Ingredient, CountIngredient , DENSE_RANK() OVER(ORDER BY Recipes.Name ) AS Iterator FROM	
 		Recipes INNER JOIN CTE_Categories AS c1 ON Recipes.CategoryId = c1.Id,
 		Ingredients INNER JOIN IngredientsInRecipe AS IR1 ON IR1.IngredientId = Ingredients.Id
-		WHERE IR1.RecipeId = Recipes.Id AND c1.ParentId = 2 AND c1.LevelLayer<2
+		WHERE IR1.RecipeId = Recipes.Id AND c1.ParentId = 3 AND c1.LevelLayer<2
 		GROUP BY  Recipes.Name, Ingredients.Name , CountIngredient 
                     ) AS t
                     WHERE Iterator<=3
