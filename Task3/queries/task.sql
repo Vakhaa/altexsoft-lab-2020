65e83f47fd4cc7d531d9a3bd4f4fe330ad1b8f6f
Denys Vynohradnyi <denv0808@gmail.com>
2020-10-02 13:29:17 +0300

    Change queries

--- Task3/queries/task.sql
+++ Task3/queries/task.sql
@@ -11,10 +11,9 @@
 )
 SELECT Recipe,Ingredient, CountIngredient, Iterator FROM (
         SELECT Recipes.Name As Recipe , Ingredients.Name As Ingredient, CountIngredient , DENSE_RANK() OVER(ORDER BY Recipes.Name ) AS Iterator FROM	
-		Recipes INNER JOIN Categories AS c ON Recipes.CategoryId = c.Id,
-        Categories INNER JOIN CTE_Categories AS c1 ON Categories.ParentId = c1.Id,
+		Recipes INNER JOIN CTE_Categories AS c1 ON Recipes.CategoryId = c1.Id,
 		Ingredients INNER JOIN IngredientsInRecipe AS IR1 ON IR1.IngredientId = Ingredients.Id
-		WHERE IR1.RecipeId = Recipes.Id AND c1.Id = 3 AND c1.LevelLayer<2
+		WHERE IR1.RecipeId = Recipes.Id AND c1.ParentId = 2 AND c1.LevelLayer<2
 		GROUP BY  Recipes.Name, Ingredients.Name , CountIngredient 
                     ) AS t
                     WHERE Iterator<=3
