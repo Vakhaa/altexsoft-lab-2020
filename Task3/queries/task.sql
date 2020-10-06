f22743a9fac72db2b5be77b26aca3fa43517a023
Denys Vynohradnyi <denv0808@gmail.com>
2020-10-06 15:48:13 +0300

    Change where in CTE_Categories

--- Task3/queries/task.sql
+++ Task3/queries/task.sql
@@ -2,7 +2,7 @@
 (  
     SELECT Id, ParentId, Name, 0 AS LevelLayer  
     FROM Categories
-		WHERE ParentId IS NULL  
+		WHERE ParentId =3
     UNION ALL  
     SELECT c1.Id, c1.ParentId, c1.Name, LevelLayer + 1  
     FROM Categories AS c1  
@@ -13,6 +13,5 @@
 SELECT Recipe, Ingredients.Name As Ingredient, CountIngredient , Iterator FROM (
         SELECT Recipes.Id AS Id, Recipes.Name As Recipe , ROW_NUMBER() OVER(ORDER BY Recipes.Name ) AS Iterator FROM	
 		Recipes INNER JOIN CTE_Categories AS c1 ON Recipes.CategoryId = c1.Id
-		WHERE c1.ParentId = 3
 ) AS t ,Ingredients INNER JOIN IngredientsInRecipe AS IR1 ON IR1.IngredientId = Ingredients.Id
 WHERE Iterator<=3 AND IR1.RecipeId = t.Id
