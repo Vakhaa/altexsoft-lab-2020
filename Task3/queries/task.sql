e89c61dba50b718f035e330a79a988ce456ec776
Denys Vynohradnyi <denv0808@gmail.com>
2020-10-08 11:59:27 +0300

    Added inner join on IngredientsInRecipe and on Ingredients

--- Task3/queries/task.sql
+++ Task3/queries/task.sql
@@ -10,8 +10,11 @@
         ON c1.ParentId = c2.Id   
 )
 
-SELECT Recipe,I.Name As Ingredient, CountIngredient , Iterator FROM (
+SELECT Recipe,I.Name As Ingredient, CountIngredient , Iterator FROM 
+(
         SELECT  R.Id AS Id, R.Name As Recipe ,ROW_NUMBER() OVER(ORDER BY R.Name ) AS Iterator FROM
         Recipes as R INNER JOIN CTE_Categories AS c1 ON R.CategoryId = c1.Id
-) AS t, Ingredients as I, IngredientsInRecipe AS IR1
-WHERE Iterator<=3 AND IR1.RecipeId = t.Id AND IR1.IngredientId = I.Id
+) 
+AS t INNER JOIN IngredientsInRecipe AS IR1 ON IR1.RecipeId = t.Id 
+INNER JOIN Ingredients as I ON IR1.IngredientId = I.Id
+WHERE Iterator<=3
